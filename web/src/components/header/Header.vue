9730dcfa2911c2067437f9d3a398ffaf7d200b0e
tomazmm <tomaz.mesarec@live.com>
2020-05-04 23:53:09 +0200

    Initialized Vue Project

---
+++ web/src/components/header/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="navigation" toggleable="lg" type="dark" variant="info">
+        <b-navbar>
+             <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        </b-navbar>
+    </div>
+</template>
+
+
+<style lang="sccs" scoped src="./header.scss"></style>
---
+++ web/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <Header />
+    wadup boy
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+import Header from '@/components/header/Header.vue'
+
+import IconService from 'icon-sdk-js'
+
+@Component({
+  components: {
+    Header,
+    IconService
+  }
+})
+export default class Home extends Vue {
+  created(){
+    const provider = new IconService.HttpProvider('https://bicon.net.solidwallet.io/api/v3');
+    const iconService = new IconService(provider);
+    //console.log(provider);
+    
+    
+    //const { IconWallet } = IconService;
+    //#const wallet = IconWallet.create()
+    //console.log(wallet.getAddress());
+    
+    
+  }
+  
+}
+</script>
