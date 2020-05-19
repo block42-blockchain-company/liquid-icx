351d92191541d0e417d1a1e0d66f045dccdc42ab
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-05-19 13:41:18 +0200

    Moved wallet specific funtion to IconWallet component

--- web/src/components/iconwallet/iconWallet.ts
+++ web/src/components/iconwallet/iconWallet.ts
@@ -1,6 +1,7 @@
 import { Mixins, Component } from 'vue-property-decorator'
-import {IconMixin} from '@/mixins/IconMixin.ts';
 import {mapGetters, mapMutations} from "vuex";
+import store from "@/store";
+import {IconMixin} from "@/mixins/IconMixin";
 
 @Component({
     computed: mapGetters({ wallet : 'getWallet'}),
@@ -8,4 +9,43 @@ import {mapGetters, mapMutations} from "vuex";
 })
 export default class IconWallet extends Mixins(IconMixin)
 {
+    created(){
+        window.addEventListener('ICONEX_RELAY_RESPONSE', this.eventHandler)
+    }
+
+    eventHandler( e: any) {
+        const {type, payload} = e.detail;
+        store.getters.isDev && console.log(type, " : ", payload)
+        if( type == "RESPONSE_HAS_ACCOUNT"){
+            if( payload ) this.hasAddress()
+            else alert("No Acccount") // TODO we need to find a better way to infrm user
+        }
+        else if ( type == "RESPONSE_HAS_ADDRESS" ){
+            if( payload ) this.requestAddress()
+            else alert("No Address")
+        }
+        else if( type == "RESPONSE_ADDRESS" ){
+            store.commit('setWallet', payload)
+        }
+    }
+
+    hasAccount(){
+        this.dispatchEvent("REQUEST_HAS_ACCOUNT", null)
+    }
+
+    hasAddress(){
+        this.dispatchEvent("REQUEST_HAS_ADDRESS", null)
+    }
+
+    requestAddress(){
+        this.dispatchEvent("REQUEST_ADDRESS", null)
+    }
+
+    dispatchEvent(type_: string, payload_: any){
+        const customEvent = new CustomEvent('ICONEX_RELAY_REQUEST', {detail: {
+                type: type_,
+                payload: payload_
+            }});
+        window.dispatchEvent(customEvent);
+    }
 }
