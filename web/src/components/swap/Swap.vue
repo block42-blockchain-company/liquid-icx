7d04166c5b80d82178f130c3d233124a2ebbc4e5
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-05-26 16:34:43 +0200

    stashed stuff from master, because im an idiot

--- web/src/components/swap/Swap.vue
+++ web/src/components/swap/Swap.vue
@@ -5,8 +5,8 @@
                    :title-link-class="'tab-title'"
                    v-for="(key, value) in pairs"
                    v-bind:key="'tab-' + key"
-                   :title="key + ' -> ' + value" :active="(key == 'ICX')">
-                <div class="mt-5 swap-form container-fluid">
+                   :title="key + ' -> ' + value" :active="(key === 'ICX')">
+                <b-form class="mt-5 swap-form container-fluid">
                     <div class="row mb-5">
                         <div class="col-sm-6">
                             <b-form-input class="amount-input"
@@ -16,13 +16,15 @@
                         </div>
                         <div class="col-sm-6">
                             <div class="amount-mirror mt-2">
-                                {{ (amount === "") ? "Amount " + value  : amount + " " +value }}
+                                {{ (amount === "") ? "Amount " + value : amount + " " +value }}
                             </div>
                         </div>
                     </div>
-                </div>
+                    <b-button :disabled="((wallet == null) || (amount === ''))"
+                              class="btn-convert mb-4"
+                              @click="join" variant="primary">Convert</b-button>
+                </b-form>
             </b-tab>
-            <b-button class="btn-convert mb-4" variant="primary">Convert</b-button>
         </b-tabs>
     </div>
 </template>
--- web/src/mixins/IconMixin.ts
+++ web/src/mixins/IconMixin.ts
@@ -1,6 +1,6 @@
 import {Component, Vue } from "vue-property-decorator";
 import IconService from 'icon-sdk-js'
-import store from '../store/index'
+
 
 @Component({
     components: {
@@ -11,44 +11,48 @@ import store from '../store/index'
 export class IconMixin extends Vue {
     public readonly provider = new IconService.HttpProvider('https://bicon.net.solidwallet.io/api/v3');
     public readonly iconService = new IconService(this.provider);
+    //public readonly iconBuilder = new this.iconService.IconBuilder();
+    public readonly licx_score_address = "cx4322ccf1ad0578a8909a162b9154170859c913eb"
 
-    created(){
-        window.addEventListener('ICONEX_RELAY_RESPONSE', this.eventHandler)
+
+    async getBalances(address ){
+        const icxBalance = await this.iconService.getBalance(address).execute();
+        const licxBalance = await this.iconService.sendTransaction(this.buildTransaction({
+            write: false,
+            method: "totalSupply",
+            params: {}
+        })).execute()
+        console.log(licxBalance)
+        return BigInt(icxBalance["c"].join(''))
     }
 
-    eventHandler( e: any) {
-        const {type, payload} = e.detail;
-        store.getters.isDev && console.log(type, " : ", payload)
-        if( type == "RESPONSE_HAS_ACCOUNT"){
-            if( payload ) this.hasAddress()
-            else alert("No Acccount") // TODO we need to find a better way to infrm user
+    async getLicxApi(){
+        const apiList = await this.iconService.getScoreApi(this.licx_score_address).execute();
+        return apiList.getList()
+    }
+
+    buildTransaction(_: Record<string, any>){
+        let tx;
+        /*
+        if(_.write){
+            tx = new this.iconBuilder.CallTransactionBuilder()
+                                .from(_.from)
+                                .method(_.method)
+                                .value(_.value)
+
         }
-        else if ( type == "RESPONSE_HAS_ADDRESS" ){
-            if( payload ) this.requestAddress()
-            else alert("No Address")
+        else{
+            new this.iconBuilder.CallBuilder()
+                .to(this.licx_score_address)
+                .method(_.method)
+                .params(_.params)
+                .build()
         }
-        else if( type == "RESPONSE_ADDRESS" ){
-            store.commit('setWallet', payload)
-        }
+        */
+        return tx
     }
 
-    hasAccount(){
-        this.dispatchEvent("REQUEST_HAS_ACCOUNT", null)
-    }
-
-    hasAddress(){
-        this.dispatchEvent("REQUEST_HAS_ADDRESS", null)
-    }
-
-    requestAddress(){
-        this.dispatchEvent("REQUEST_ADDRESS", null)
-    }
-
-    dispatchEvent(type_: string, payload_: any){
-        const customEvent = new CustomEvent('ICONEX_RELAY_REQUEST', {detail: {
-                type: type_,
-                payload: payload_
-            }});
-        window.dispatchEvent(customEvent);
+    async sendTransaction(){
+        //
     }
 }
