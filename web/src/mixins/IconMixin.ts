1c032edacbaa17ed1fee128f64561321f4e8e8a1
Tomaž Mesarec <tomaz.mesarec@block42.tech>
2020-05-29 12:27:01 +0200

    Feature/swap (#7)
    * Does not reload conveert button, when changing tab
    
    * stashed stuff from master, because im an idiot
    
    * getting licx and icx balance
    
    * PR resolved reqeusts

--- web/src/mixins/IconMixin.ts
+++ web/src/mixins/IconMixin.ts
@@ -1,54 +1,49 @@
 import {Component, Vue } from "vue-property-decorator";
-import IconService from 'icon-sdk-js'
-import store from '../store/index'
+import IconService, { IconBuilder } from 'icon-sdk-js'
+
 
 @Component({
     components: {
-        IconService
     }
 })
 
 export class IconMixin extends Vue {
     public readonly provider = new IconService.HttpProvider('https://bicon.net.solidwallet.io/api/v3');
     public readonly iconService = new IconService(this.provider);
+    public readonly licx_score_address = "cx4322ccf1ad0578a8909a162b9154170859c913eb"
 
-    created(){
-        window.addEventListener('ICONEX_RELAY_RESPONSE', this.eventHandler)
-    }
+    async getBalances(address ){
+        const icxBalance = await this.iconService.getBalance(address).execute();
+        const licxBalance = await this.iconService.call(this.buildTransaction({
+            write: false,
+            method: "balanceOf",
+            params: {_owner: address}
+        })).execute()
 
-    eventHandler( e: any) {
-        const {type, payload} = e.detail;
-        store.getters.isDev && console.log(type, " : ", payload)
-        if( type == "RESPONSE_HAS_ACCOUNT"){
-            if( payload ) this.hasAddress()
-            else alert("No Acccount") // TODO we need to find a better way to infrm user
-        }
-        else if ( type == "RESPONSE_HAS_ADDRESS" ){
-            if( payload ) this.requestAddress()
-            else alert("No Address")
-        }
-        else if( type == "RESPONSE_ADDRESS" ){
-            store.commit('setWallet', payload)
+        return {
+            icx: BigInt(icxBalance["c"].join('')),
+            licx: BigInt(licxBalance)
         }
     }
 
-    hasAccount(){
-        this.dispatchEvent("REQUEST_HAS_ACCOUNT", null)
+    async getLicxApi(){
+        const apiList = await this.iconService.getScoreApi(this.licx_score_address).execute();
+        return apiList.getList()
     }
 
-    hasAddress(){
-        this.dispatchEvent("REQUEST_HAS_ADDRESS", null)
-    }
+    buildTransaction(_: Record<string, any>){
+        const { CallBuilder } = IconBuilder;
 
-    requestAddress(){
-        this.dispatchEvent("REQUEST_ADDRESS", null)
-    }
-
-    dispatchEvent(type_: string, payload_: any){
-        const customEvent = new CustomEvent('ICONEX_RELAY_REQUEST', {detail: {
-                type: type_,
-                payload: payload_
-            }});
-        window.dispatchEvent(customEvent);
+        let tx = null;
+        if(!_.write){
+            tx = new CallBuilder()
+                .to(this.licx_score_address)
+                .method(_.method)
+                .params(_.params)
+                .build()
+        }
+        else{
+        }
+        return tx
     }
 }
