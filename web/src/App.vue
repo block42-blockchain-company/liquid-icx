2306ef654d29fad8bd759bec17ddc4fc361599b6
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-06-17 13:02:24 +0200

    Added scss base classes

--- web/src/App.vue
+++ web/src/App.vue
@@ -10,14 +10,35 @@
 
 
 body{
-
   margin: 0;
   #app {
+    /*Base styling*/
     font-family: 'Open Sans', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #1BAABA;
+
+    /* Base input class*/
+    .input-licx {
+      border: none;
+      border-bottom: 2px solid $background-base-washed;
+      text-align: center;
+      font-size: 1.5em;
+    }
+
+    /*Base btn class*/
+    .btn-licx {
+      border: none;
+      border-radius: 0;
+      font-size: 1.3em;
+      background: $icx-base;
+      &:disabled{
+        cursor: not-allowed;
+        pointer-events: all !important;
+      }
+    }
+
   }
 }
 </style>
