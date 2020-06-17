4291a40977b1d856fbeb68bf913a0d22a2ccb06a
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-06-17 16:50:29 +0200

    Building join tx

--- web/src/components/swap/swap.ts
+++ web/src/components/swap/swap.ts
@@ -1,7 +1,7 @@
 import {Component , Mixins} from 'vue-property-decorator'
 import {IconMixin} from "@/mixins/IconMixin";
 import {mapGetters} from "vuex";
-
+import {IconConverter, IconAmount} from 'icon-sdk-js'
 
 @Component({
     components: {},
@@ -29,10 +29,24 @@ export default class Swap extends Mixins(IconMixin) {
         const tx = this.buildTransaction({
             write: true,
             method: "join",
+            steps: 200000,
             from: this.wallet.address,
             params: {},
-            value: Number(this.amount)
+            value: IconAmount.of(this.amount, IconAmount.Unit.ICX).convertUnit(IconAmount.Unit.LOOP)
         })
 
+        window.dispatchEvent(new CustomEvent('ICONEX_RELAY_REQUEST', {
+            detail: {
+                type: 'REQUEST_JSON-RPC',
+                payload: {
+                    jsonrpc: "2.0",
+                    method: "icx_sendTransaction",
+                    params: IconConverter.toRawTransaction(tx),
+                    id: 50889
+                }
+            }
+        }));
+
+        //this.getBalances(this.wallet.address)
     }
 }
