335836a945db28a8383f0dec69c867bf0f44f2a6
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-06-18 16:24:14 +0200

    Fix

--- web/src/components/transfer/transfer.ts
+++ web/src/components/transfer/transfer.ts
@@ -27,22 +27,20 @@ export default class Transfer extends Mixins(IconMixin) {
       return;
     }
 
-    const tx = this.buildTransaction({
-      write: true,
-      method: "transfer",
-      steps: 200000,
-      from: this.wallet.address,
-      params: {"_to": this.to_address,
-               "_value": IconConverter.toHex(IconAmount.of(this.to_amount, IconAmount.Unit.ICX).toLoop())},
-    })
-
     window.dispatchEvent(new CustomEvent('ICONEX_RELAY_REQUEST', {
       detail: {
         type: 'REQUEST_JSON-RPC',
         payload: {
           jsonrpc: "2.0",
           method: "icx_sendTransaction",
-          params: IconConverter.toRawTransaction(tx),
+          params: IconConverter.toRawTransaction(this.buildTransaction({
+            write: true,
+            method: "transfer",
+            steps: 200000,
+            from: this.wallet.address,
+            params: {"_to": this.to_address,
+              "_value": IconConverter.toHex(IconAmount.of(this.to_amount, IconAmount.Unit.ICX).toLoop())},
+          })),
           id: 50889
         }
       }
