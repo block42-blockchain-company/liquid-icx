2f6ec2014476e7750ab8ce765a5428b90807f401
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-06-29 13:59:13 +0200

    Added style to footer

--- web/src/components/footer/Footer.vue
+++ web/src/components/footer/Footer.vue
@@ -1,10 +1,10 @@
 <template>
     <b-container class="footer" fluid>
-        <b-row>
-            <b-col>
-                <router-link to="/about">About LICX</router-link>
+        <b-row align-h="around">
+            <b-col cols="2">
+                <router-link class="link" to="/about">About LICX</router-link>
             </b-col>
-            <b-col>
+            <b-col cols="3" class="general-info">
                 ICX locked in LICX: <span>988123.1231</span>
             </b-col>
         </b-row>
