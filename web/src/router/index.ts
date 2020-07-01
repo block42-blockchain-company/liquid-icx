99a4d62ddf003547d4ecc681adb916b8fd132b80
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2020-07-01 15:12:58 +0200

    Meta-Data change

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -9,7 +9,10 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: "LICX"
+    }
   },
   {
     path: '/about',
@@ -17,7 +20,10 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: "LICX - About"
+    }
   },
   {
     /* -- 404 --*/
