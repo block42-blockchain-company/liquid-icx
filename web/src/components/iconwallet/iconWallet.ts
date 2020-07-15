ff51734e302c5d4d88952cf363f93bd0ee5fa83e
Tomaž Mesarec <tomaz.mesarec@block42.tech>
2020-07-15 14:09:06 +0200

    Splitting score and web directories into two separate repos (#21)

--- web/src/components/iconwallet/iconWallet.ts
+++
@@ -1,8 +0,0 @@
-import { Mixins, Component } from 'vue-property-decorator'
-import {IconMixin} from "@/mixins/IconMixin";
-
-@Component({
-})
-export default class IconWallet extends Mixins(IconMixin)
-{
-}
