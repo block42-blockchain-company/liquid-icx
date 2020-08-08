8877e02c0c315f7fcf83e7c5c0dd42015cd0a5d7
Paul to the Plessing <paul.plessing@block42.tech>
2020-08-08 16:45:33 +0700

    added system_score_interface again so that the Holder class has access

--- score/liquid_icx/scorelib/Utils.py
+++ score/liquid_icx/scorelib/Utils.py
@@ -4,6 +4,10 @@ from ..interfaces.system_score_interface import *
 
 class Utils:
     @staticmethod
+    def system_score_interface():
+        return IconScoreBase.create_interface_score(ZERO_SCORE_ADDRESS, InterfaceSystemScore)
+
+    @staticmethod
     def remove_from_array(array: ArrayDB, el) -> None:
         temp = []
         # find that element and remove it
